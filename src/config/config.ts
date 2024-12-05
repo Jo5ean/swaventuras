a081f8eceeb57b0118e13873ac40424a33cec3b6
Jo5ean <josean.urbani@gmail.com>
2024-12-05 15:54:25 -0300

    initializing

---
+++ src/config/config.ts
@@ -0,0 +1,11 @@
+const config = {
+    title: "Agroabesan - Filtros y Herramientas para Maquinaria Pesada",
+    site_name: "Agroabesan",
+    description: "Empresa familiar con 15 años de experiencia en filtros y herramientas para maquinaria pesada. Especialistas en soluciones para sectores agrícola, minero, industrial y construcción. Garantizamos calidad y eficiencia en todos nuestros productos.",
+    image: "/images/fachada-empresa.webp",
+    theme_color: "#009bce",
+    keywords: "filtros maquinaria pesada, herramientas industriales, sector agrícola, minería, construcción, filtros industriales, maquinaria pesada, repuestos maquinaria, asesoría técnica, filtros a medida",
+    author: "@agroabesan",
+    email: "soporte@agroabesan.com",
+}
+export default config;
